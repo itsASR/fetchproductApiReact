210b30549880338c3c4a75aa4002d241482ea736
itsASR <asreyanshsharma@gmail.com>
2023-08-25 08:21:02 +0530

    rav

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+import './index.css'
+// import Inputstake from './Inputstake.jsx'
+import App from './slider/App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+    {/* <Inputstake></Inputstake> */}
+  </React.StrictMode>,
+)
